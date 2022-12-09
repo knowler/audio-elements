2cd5631fc4646376e4c2502a637ffe4174b4bc6d
Nathan Knowler <nathan@knowler.me>
2022-12-09 15:40:56 -0600

    Add performance marks

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ const customElements = [
 	//'convolver-node',
 ];
 
+performance.mark('begin waiting for custom elements to be defined');
 await Promise.all(
 	customElements.map(
 		customElement => window.customElements.whenDefined(customElement),
 	),
 );
-console.log('custom elements defined');
+performance.mark('custom elements have been defined');
 document.body.appendChild(
 	document.querySelector('template').content.cloneNode(true),
 );
