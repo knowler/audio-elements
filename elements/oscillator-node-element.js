1150aedb3da2b9f64f7316bef43b8af5ae1cfa37
Nathan Knowler <nathan@knowler.me>
2022-12-12 10:48:47 -0600

    Refactor oscillator node

--- elements/oscillator-node-element.js
+++ elements/oscillator-node-element.js
@@ -1,67 +1,22 @@
 import { BaseAudioNodeElement } from './base-audio-node-element.js';
-import {relativeURL} from './utils.js';
+import { relativeURL } from './utils.js';
+
+const TYPES = ['sine', 'square', 'sawtooth', 'triangle'];
 
 export class OscillatorNodeElement extends BaseAudioNodeElement {
-	get waveform() {
-		return this.getAttribute('waveform') ?? undefined;
-	}
-	set waveform(value) {
-		this.setAttribute('waveform', value);
-		if (this.node) this.node.type = value;
-	}
-	get frequency() { return this.getAttribute('frequency') ?? undefined; }
-	set frequency(value) {
-		this.setAttribute('frequency', value);
-		this.node?.frequency.setValueAtTime(
-			Number(value),
-			this.context.currentTime,
-		);
-	}
-	get detune() { return this.getAttribute('detune') ?? undefined; }
-	set detune(value) {
-		this.setAttribute('detune', value);
-		this.node?.detune.setValueAtTime(
-			Number(value),
-			this.context.currentTime,
-		);
-	}
-
-	started = false;
-	async start() {
-		await this.context.resume();
-		this.node?.stop();
-		this.node = new OscillatorNode(this.context, {
-			type: this.waveform,
-			frequency: this.frequency,
-			detune: this.detune,
-		});
-		this.node.connect(this.destination);
-		this.node.start();
-		this.started = true;
-	}
-
-	stop() {
-		this.node.stop();
-		this.node = undefined;
-		this.started = false;
-	}
-
 	template = html => html`
 		<link rel="stylesheet" href="${relativeURL('oscillator-node-element.css')}">
 		<fieldset>
 			<legend>Oscillator</legend>
 			<label>
-				Waveform
-				<select name="waveform">
-					<option ${!this.waveform || this.waveform === "sine" ? "selected" : ""} value="sine">Sine</option>
-					<option ${this.waveform === "square" ? "selected" : ""} value="square">Square</option>
-					<option ${this.waveform === "sawtooth" ? "selected" : ""} value="sawtooth">Sawtooth</option>
-					<option ${this.waveform === "triangle" ? "selected" : ""} value="triangle">Triangle</option>
+				Type
+				<select name="type">
+					${TYPES.map(type => html`<option${type === this.type ? " selected" : ""}>${type}</option>`).join('')}
 				</select>
 			</label>
 			<label>
 				Frequency
-				<input name="frequency" type="range" min="0" max="3000" value="${this.frequency ?? 440}">
+				<input name="frequency" type="range" min="0" max="3000" value="${this.frequency}">
 			</label>
 			<label>
 				Detune
@@ -77,51 +32,25 @@ export class OscillatorNodeElement extends BaseAudioNodeElement {
 	connectedCallback() {
 		super.connectedCallback();
 
-		const removeOnDisconnect = {signal: this.disconnectedSignal};
-		const {waveform, frequency, detune, toggle, remove} = this.controlElements;
-		frequency.addEventListener('input', this.#handleControlEvent.bind(this), removeOnDisconnect);
-		detune.addEventListener('input', this.#handleControlEvent.bind(this), removeOnDisconnect);
-		waveform.addEventListener('input', this.#handleControlEvent.bind(this), removeOnDisconnect);
-		toggle.addEventListener('click', this.#handleControlEvent.bind(this), removeOnDisconnect);
-		remove.addEventListener('click', this.#handleControlEvent.bind(this), removeOnDisconnect);
-	}
-
-	static observedAttributes = ['waveform', 'frequency', 'detune'];
-	attributeChangedCallback(name, oldValue, newValue) {
-		if (!this.isConnected || !this.context || oldValue === newValue) return;
-
-		switch(name) {
-			case 'frequency':
-				this.frequency = Number(newValue);
-				this.controlElements.namedItem('frequency').value = this.frequency;
-				break;
-			case 'detune':
-				this.detune = Number(newValue);
-				this.controlElements.namedItem('detune').value = this.detune;
-				break;
-			case 'waveform':
-				this.waveform = newValue;
-				this.controlElements.namedItem('waveform').value = this.waveform;
-				break;
+		for (const control of this.controlElements) {
+			control.addEventListener(
+				control.name === 'toggle' || control.name === 'remove' ? 'click' : 'input',
+				this.#handleControlEvent.bind(this),
+				{ signal: this.disconnectedSignal },
+			);
 		}
 	}
 
 	#handleControlEvent(event) {
 		switch (event.target.name) {
+			case 'type':
+				this.type = event.target.value;
+				break;
 			case 'frequency':
 			case 'detune':
 				this[event.target.name] = Number(event.target.value);
 				break;
-			case 'waveform':
-				this.waveform = event.target.value;
-				break;
 			case 'toggle':
-				event.currentTarget.setAttribute(
-					'aria-checked',
-					JSON.stringify(!JSON.parse(
-						event.currentTarget.getAttribute('aria-checked')
-					)),
-				);
 				if (this.started) this.stop();
 				else this.start();
 				break;
@@ -130,6 +59,75 @@ export class OscillatorNodeElement extends BaseAudioNodeElement {
 				break;
 		}
 	}
+
+	static observedAttributes = ['type', 'frequency', 'detune'];
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (oldValue === newValue) return;
+		// Set normalized value and update AudioNode value.
+		let normalized;
+		switch (name) {
+			case 'type':
+				normalized = newValue ?? 'sine';
+				if (this.node) this.node.type = normalized;
+				break;
+			case 'frequency':
+				normalized = newValue ? Number(newValue) : 440;
+				this.node?.frequency.setValueAtTime(normalized, this.context.currentTime);
+				break;
+			case 'detune':
+				normalized = newValue ? Number(newValue) : 0;
+				this.node?.detune.setValueAtTime(normalized, this.context.currentTime);
+				break;
+		}
+		// Update control
+		this.controlElements.namedItem(name).value = normalized;
+	}
+	get type() {
+		return this.getAttribute('type') ?? 'sine';
+	}
+	set type(value) {
+		this.setAttribute('type', value);
+	}
+	get frequency() {
+		return this.getAttribute('frequency') ?? 440;
+	}
+	set frequency(value) {
+		this.setAttribute('frequency', value);
+	}
+	get detune() {
+		return this.getAttribute('detune') ?? 0;
+	}
+	set detune(value) {
+		this.setAttribute('detune', value);
+	}
+
+	get started() {
+		return JSON.parse(this.controlElements.toggle.getAttribute('aria-checked'));
+	}
+	set started(value) {
+		this.controlElements.toggle.setAttribute('aria-checked', JSON.stringify(value));
+	}
+	async start() {
+		if (!this.isConnected) return;
+		await this.context.resume();
+		this.node?.stop();
+		this.node = new OscillatorNode(this.context, {
+			type: this.type,
+			frequency: this.frequency,
+			detune: this.detune,
+		});
+		this.node.connect(this.destination);
+		this.node.start();
+		this.started = true;
+	}
+	stop() {
+		if (!this.isConnected) return;
+		this.node.stop();
+		this.node.disconnect();
+		delete this.node;
+		this.started = false;
+	}
+
 }
 
 if (!window.customElements.get('oscillator-node')) {
